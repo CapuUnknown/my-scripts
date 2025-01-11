f7703ecb0b946bcd8385d3eb3c3c47083763d9e4
CapuUnknown <geilkecapu@gmail.com>
2025-01-11 20:33:27 +0100

    rename & del

--- my-scripts/subs.sh
+++ my-scripts/subs.sh
@@ -2,7 +2,6 @@
 
 files=( "$HOME/Downloads/convert/"*.mkv )
 PS3='Select file, press 0 when done: '
-
 while true; do
     select file in "${files[@]}"; do
 	if [[ $REPLY == "0" ]]; then
@@ -13,16 +12,21 @@ while true; do
             echo
 	    echo 'Invalid choice, try again' >&2
        	else
-	    dir=$(dirname "$file")	# Extract dir path
+	    dir=$(dirname "$file")	# Extract dir patih
 	    base=$(basename "$file")	# Extract file name with extension
 	    name="${base%.*}"	# Remove extension
 	    ext="${base##*.}"	# Extract extension
 	    new_file="subbed_${name}.${ext}"
       new_name="${name}.${ext}"
-	    ffmpeg -i "$new_name" -vf subtitles="$new_name" "$new_file" > /dev/null 2>&1
+      renamed="waste"
+      renamed="$renamed.${ext}"
+      mv "$new_name" "$renamed"
+	    ffmpeg -i "$renamed" -vf subtitles="$renamed" "$new_file" > /dev/null 2>&1
 	    echo
-	    echo 'Burning in subtitles, this may take a while'
-	    echo
+	    echo 'Succesfully burned in subtitles'
+      rm waste.mkv
+      echo 'Deleted old .mkv file'
+      echo
             break
        	fi
     done
